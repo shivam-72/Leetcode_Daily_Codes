1b113515589f8fccb118d8eca1b2bd4b2fe2599e
Shivam Prakash <85667261+shivam-72@users.noreply.github.com>
2023-06-19 07:55:09 +0530

    Create find_the_highest_altitude.cpp

---
+++ June_Leetcode_Challenge_codes/find_the_highest_altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int altitude=0;
+        int sum=0;
+        for(int i=0;i<gain.size();i++){
+            sum+=gain[i];
+            altitude=max(altitude,sum);
+        }
+        return altitude;
+    }
+};
